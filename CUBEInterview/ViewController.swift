a06412ab02624395fef73a41511f19f24fe71857
chechenliu <qwer21508@gmail.com>
2025-03-30 11:13:40 +0800

    Initial Commit

---
+++ CUBEInterview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CUBEInterview
+//
+//  Created by CheChenLiu on 2025/3/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
